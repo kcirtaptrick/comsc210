dc769bc46646c0f77e9e9a586d506650d1df3c0d
Tony Chern <run1@aslugs.com>
2018-02-06 22:53:46 +0000

    020618

--- m02/as2_testFrac.cpp
+++ m02/as2_testFrac.cpp
@@ -1,113 +1,81 @@
-// inserting into a list
+// Illustrating user defined Data Type
+// Frac Class
 #include <iostream>
-#include <list>
-#include <sstream>
+#include <string>
+
 using namespace std;
 
+// forward declaration for friend ostream operator << 
+class Frac;
+std:: ostream &operator << (std::ostream &, const Frac &);
 
- 
-class Frac; // Forward Declaration
- 
-// Function Prototypes for Overloaded Stream Operators
-ostream &operator << (ostream &, const Frac &);
-istream &operator >> (istream &, Frac &);
- 
 class Frac {
-private:
-    int num, den; // num/den is a frac number
-public:
-    // constructors
-    Frac() {num = 0; den = 1;}
-    Frac(string s){
-        stringstream strm(s);
-        strm >> num;
-        
-        char peekchar = strm.peek();
-        if(strm && peekchar == '/') {
-            strm.get();
-            strm >> den;
-        }
-        else den = 1;
-    };
-    Frac(int num_, int den_) : num(num_), den(den_) {}
-    Frac(const Frac& rhs) : num(rhs.num), den(rhs.den) {}
-    // assignment operator
-    Frac operator=(const Frac& rhs);
-    // math + - * /
-    Frac operator + (Frac &f2) {
-        Frac temp;
-        temp.num = num*f2.den + den*f2.num;
-        temp.den = den*f2.den;
-        return temp;
-    }
-    Frac operator - (Frac &f2);
-    Frac operator * (Frac &f2);
-    Frac operator / (Frac &f2);
-    // increment ++ decrement --
-    Frac operator++();
-    Frac operator++(int);
-    Frac operator--();
-    Frac operator--(int);
-    // comparators
-    bool operator == (Frac &f2);
-    bool operator != (Frac &f2);
-    bool operator < (Frac &f2);
-    bool operator > (Frac &f2);
-    bool operator <= (Frac &f2);
-    bool operator >= (Frac &f2);
- 
-    // type cast
-    double get_value();
- 
-    //stream extraction operators
-    friend istream& operator>>(istream& strm, Frac& f);
- 
-    // stream insertion operator overload
+// private:
+    long num;
+    long den;
 
-	friend ostream& operator<<(ostream& strm,const Frac& f){
-	    strm << f.num << '/' << f.den;
-	    return strm;
-    } 
- 
-    // auxiliary method
-    Frac lowterms(Frac &f); //arg reduced to lowest terms
+public:
+    Frac() {num=0; den=1;};
+    Frac(long n) {num=n; den=1;};
+    Frac(long n, long d) {num=n; den=d;};
+    Frac(const Frac &x) {
+        num = x.num; den = x.den;
+    };
+    Frac& operator = (const Frac &x) { 
+        num = x.num; den = x.den;
+    };
+
+    Frac operator + (const Frac &x) {
+        Frac temp;
+        temp.num = num*x.den + x.num*den; 
+        temp.den = den*x.den;
+        return temp;
+    };
+    
+    Frac operator / (const Frac &x) {
+        Frac temp;
+        temp.num = num * x.den; 
+        temp.den = den * x.num;
+        return temp;
+    };
+    bool operator > (const Frac &x) {
+        return (num*x.den > den*x.num) ? true : false;
+    }
+
+    // Three console output methods
+    // method 1, using cout (an OO principle violation)
+    void show() { std::cout << num << "/" << den ; }
+    
+    // method 2, overloading the ostream << operator definition
+    friend std::ostream &operator << (std::ostream &strm, const Frac &right){
+        strm << right.num << "/" << right.den;
+        return strm;
+    }
+    
+    // method 3, convert the object data type to a string
+    std::string toString() { 
+        return std::to_string(num) + "/" + std::to_string(den);
+    }
 };
 
-
-
-
-int main () {
-    list<double> mylist = {5.4, 6.8, 8.1};
-    list<double>::iterator it;
- 
-    for(auto item:mylist) cout << ' ' << item;// 5.4, 6.8, 8.1
- 
-    it = mylist.begin();
-    ++it;       // it points now to 2nd number
- 
-    mylist.insert (it,2.3);     // 5.4, 2.3, 6.8, 8.1
- 
-    mylist.insert (it,2,7.8);   // 5.4, 2.3, 7.8, 7.8, 6.8, 8.1                                    // 1 10 20 30 30 20 2 3 4 5
- 
-    cout << "\nmylist contains:\n";
-    for(auto item:mylist) cout << ' ' << item;
-    cout << '\n';
- 
-    double sum = 0;
-    while (!mylist.empty()){
-        sum += mylist.back();
-        mylist.pop_back();
-    }
-    cout << "Sum is " << sum << endl;
-    for(auto item:mylist) cout << ' ' << item;
-    cout << endl;
+main() {
+    // using method 1 show() to dispplay object content
+    // exercise constructors and operator =
+    Frac x(3,4);
+    cout << "Create x: "; x.show();
+    Frac y(2,3);
+    cout << "\nCreate y: "; y.show();
+    Frac z(x);
+    cout << "\nCreate z from x: "; z.show();
+    z = y; 
+    cout << "\nAssign z as y: "; z.show();
     
-    // Frac number testing
-    Frac f1("2/3"), f2("3/4");
-    list<Frac> flist;
-    flist.insert(f1);
-    flist.insert(f2);
-    for(auto item:flist) cout << ' ' << item;
-    
-    return 0;
+    // using method 2 operator <<
+    cout << "\nx+y: " << x << " + " << y << " is " << x+y;
+    cout << "\nx/y: " << x << " / " << y << " is " << x/y << endl;
+    cout << x << " is " << ((x>y) ? "":"not ") << "greater than " << y << endl;
+    cout << y << " is " << ((y>x) ? "":"not ") << "greater than " << x;
+
+    // using method 3 toString()
+    cout << "\nx: " << x.toString() << "\ny: " << y.toString();
 }
